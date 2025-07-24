a08e369e131f7f99a7cd6f54073731df4f191081
noeyigg <miyg2003@naver.com>
2025-07-24 17:34:46 +0900

    feat: 인증상태 확인 후 리다이렉트 기능

--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,22 @@
-import './globals.css'
-import { AuthProvider } from './context/AuthContext'
+import "./globals.css";
+import { AuthProvider } from "./context/AuthContext";
+import Link from "next/link";
 
 export default function RootLayout({ children }) {
   return (
-
     <html lang="ko">
       <body className="px-4 md:px-20">
         {/* 로그인 전역 상태 유지 */}
         <AuthProvider>
-        <header className="flex items-center justify-center gap-3 py-10">
-          <img src="/logo.png" alt="Logo" className="h-10" />
-          <h1 className="text-2xl font-bold text-blue-800">Among Us</h1>
-        </header>
-        {children}
+          <Link href={`/`}>
+            <header className="flex items-center justify-center gap-3 py-10">
+              <img src="/logo.png" alt="Logo" className="h-10" />
+              <h1 className="text-2xl font-bold text-blue-800">Among Us</h1>
+            </header>
+          </Link>
+          {children}
         </AuthProvider>
       </body>
     </html>
-  )
+  );
 }
