d76e6112af84bc93f345ab5bdd4c01269d43e6e6
noeyigg <miyg2003@naver.com>
2025-07-24 17:10:50 +0900

    feat: 라우트 수정

--- app/[postId]/page.js
+++ app/[postId]/page.js
@@ -1,18 +1,18 @@
 // app/dashboard/[postId]/page.js
 import { Suspense } from "react";
-import PostForm from "../../ui/PostForm";
-import Comment from "../../ui/Comment";
-import Spinner from "../../ui/Spinner";
+import PostForm from "../ui/PostForm";
+import Comment from "../ui/Comment";
+import Spinner from "../ui/Spinner";
 
 async function getPostData(postId) {
-  const res = await fetch(`http://localhost:3000/api/dashboard/${postId}`, {
+  const res = await fetch(`http://localhost:3000/api/${postId}`, {
     cache: "no-store",
   });
-  
+
   if (!res.ok) {
-    throw new Error('Failed to fetch post data');
+    throw new Error("Failed to fetch post data");
   }
-  
+
   const json = await res.json();
   return json;
 }
@@ -35,7 +35,7 @@ async function Comments({ postId }) {
 
 export default async function PostIdPage({ params }) {
   const { postId } = await params;
-  
+
   try {
     const { post } = await getPostData(postId);
 
@@ -47,9 +47,9 @@ export default async function PostIdPage({ params }) {
             {` - ${post.users.name}`}
           </p>
         </div>
-        
+
         <PostForm />
-        
+
         {/* 댓글 데이터 Suspense로 streaming 처리 */}
         <Suspense fallback={<Spinner />}>
           <Comments postId={postId} />
@@ -59,7 +59,9 @@ export default async function PostIdPage({ params }) {
   } catch (error) {
     return (
       <div className="my-10">
-        <p className="text-red-500">게시글을 불러오는 중 오류가 발생했습니다.</p>
+        <p className="text-red-500">
+          게시글을 불러오는 중 오류가 발생했습니다.
+        </p>
       </div>
     );
   }
